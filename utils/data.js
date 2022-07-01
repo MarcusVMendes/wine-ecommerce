ea78706583d22280fb510a1f2ae00e8a772c580f
MarcusVMendes <marcus.mendes89@gmail.com>
2022-07-01 16:24:27 -0300

    style: handling with homepage main section

--- utils/data.js
+++ utils/data.js
@@ -1,7 +1,7 @@
 export const navigationLinks = [
-    { "label": "clube", "path": "/club" },
-    { "label": "loja", "path": "/store" },
-    { "label": "produtos", "path": "/products" },
-    { "label": "ofertas", "path": "/offers" },
-    { "label": "eventos", "path": "/events" },
+    { "label": "clube", "path": "/" },
+    { "label": "loja", "path": "/" },
+    { "label": "produtos", "path": "/" },
+    { "label": "ofertas", "path": "/" },
+    { "label": "eventos", "path": "/" },
 ];
